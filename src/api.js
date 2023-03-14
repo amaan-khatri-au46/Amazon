de7c9f3db44289dfc864f680bcb65006f0dd6f36
amaan <100783579+amaan-khatri-au46@users.noreply.github.com>
2023-03-14 12:27:04 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,296 @@
+// i am defining a function to get product information
+// we are using axios to send ajax request
+// axios except all parameter to sending the request
+
+import axios from "axios";
+import { apiUrl } from "./config.js";
+import { getUserInfo } from "./localStorage.js";
+
+export const getProduct = async (id) => {
+  try {
+    const response = await axios({
+      url: `${apiUrl}/api/products/${id}`,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.statusText !== "OK") {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return { error: error.response.data.message || error.message };
+  }
+};
+
+export const signin = async ({ email, password }) => {
+  try {
+    const response = await axios({
+      url: `${apiUrl}/api/users/signin`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        email,
+        password,
+      },
+    });
+    if (response.statusText !== "OK") {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return { error: error.response.data.message || error.message };
+  }
+};
+
+export const register = async ({ name, email, password }) => {
+  try {
+    const response = await axios({
+      url: `${apiUrl}/api/users/register`,
+      method: "POST",
+      header: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+    if (response.statusText !== "OK") {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return { error: err.response.data.message || err.message };
+  }
+};
+
+export const update = async ({ name, email, password }) => {
+  try {
+    const { _id, token } = getUserInfo();
+    const response = await axios({
+      url: `${apiUrl}/api/users/${_id}`,
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+    if (response.statusText !== "OK") {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return { error: err.response.data.message || err.message };
+  }
+};
+
+export const createOrder = async (order) => {
+  try {
+    const { token } = getUserInfo();
+    const response = await axios({
+      url: `${apiUrl}/api/orders`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: order,
+    });
+    if (response.statusText !== "Created") {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    return { error: err.response ? err.response.data.message : err.message };
+  }
+};
+
+export const getOrder = async (id) => {
+  try {
+    const { token } = getUserInfo();
+    const response = await axios({
+      url: `${apiUrl}/api/orders/${id}`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.statusText !== "OK") {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    return { err: err.message };
+  }
+};
+
+ export const getMyOrders = async () => {
+   try {
+     const { token } = getUserInfo();
+     const response = await axios({
+       url: `${apiUrl}/api/orders/mine`,
+       headers: {
+         "Content-Type": "application/json",
+         Authorization: `Bearer ${token}`,
+       },
+     });
+     if (response.statusText !== 'OK') {
+       throw new Error(response.data.message);
+     }
+     return response.data;
+   } catch (err) {
+     return { error: err.response ? err.response.data.message : err.message };
+   }
+ };
+
+export const getPaypalClientId = async () => {
+  const response = await axios({
+    url: `${apiUrl}/api/paypal/clientId`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.statusText !== "OK") {
+    throw new Error(response.data.message);
+  }
+  return response.data.clientId;
+};
+
+export const payOrder = async (orderId, paymentResult) => {
+  try {
+    const { token } = getUserInfo();
+    const response = await axios({
+      url: `${apiUrl}/api/orders/${orderId}/pay`,
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: paymentResult,
+    });
+    if (response.statusText !== 'OK') {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    return { error: err.response ? err.response.data.message : err.message };
+  }
+};
+
+export const getProducts = async ({ searchKeyword = "" }) => {
+  try {
+    let queryString = "?";
+    if (searchKeyword) queryString += `searchKeyword=${searchKeyword}&`;
+
+    const response = await axios({
+      url: `${apiUrl}/api/products${queryString}`,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.statusText !== 'OK') {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return { error: err.response.data.message || err.message };
+  }
+};
+
+export const deleteProduct = async (productId) => {
+  try {
+    const { token } = getUserInfo();
+    const response = await axios({
+      url: `${apiUrl}/api/products/${productId}`,
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.statusText !== 'OK') {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    return { error: err.response.data.message || err.message };
+  }
+};
+
+
+export const createProduct = async () => {
+  try {
+    const { token } = getUserInfo();
+    const response = await axios({
+      url: `${apiUrl}/api/products`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.statusText !== "Created") {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    return { error: err.response.data.message || err.message };
+  }
+};
+
+export const getOrders = async () => {
+  try {
+    const { token } = getUserInfo();
+    const response = await axios({
+      url: `${apiUrl}/api/orders`,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.statusText !== 'OK') {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return { error: err.response.data.message || err.message };
+  }
+};
+
+export const deleteOrder = async (orderId) => {
+  try {
+    const { token } = getUserInfo();
+    const response = await axios({
+      url: `${apiUrl}/api/orders/${orderId}`,
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.statusText !== 'OK') {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    return { error: err.response.data.message || err.message };
+  }
+};
