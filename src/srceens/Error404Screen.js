a710b72489c58b929a7f0f9302a65c51697a1d4b
amaan <100783579+amaan-khatri-au46@users.noreply.github.com>
2023-03-14 12:39:00 +0530

    Delete src directory

--- src/srceens/Error404Screen.js
+++
@@ -1,8 +0,0 @@
-const Error404Screen = {
-    // object that contains a render function
-    render: () => {
-        //it just return ProductScreen 
-        return `<div>Page not Found!</div>`;
-    },
-};
-export default Error404Screen;
